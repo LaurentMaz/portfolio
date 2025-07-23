71cdb77d089dd83a8e6f2bc04879a8a56531b56b
Laurent <mazoyer.laurent@gmail.com>
2025-07-23 16:25:20 +0200

    Implement navigation bar responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
 import { assets } from "@/assets/assets";
+import { useRef } from "react";
 
 const Navbar = () => {
+  const sideMenuRef = useRef<HTMLUListElement>(null);
+  const openMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+  const closeMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(16rem)";
+    }
+  };
+
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
       <nav className="flex items-center justify-between w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 z-50">
         <a href="#top">
           <Image
@@ -12,7 +28,7 @@ const Navbar = () => {
             alt="Laurent MAZOYER logo"
           />
         </a>
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-2xl px-12 py-3 bg-white/50 shadow-sm">
           <li>
             <a href="#top" className="font-ovo">
               Accueil
@@ -39,7 +55,14 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
-        <div>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image
+              src={assets.moon_icon}
+              alt="icone lune de theme"
+              className="w-6 cursor-pointer"
+            />
+          </button>
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo"
@@ -47,7 +70,54 @@ const Navbar = () => {
             Contact
             <Image src={assets.arrow_icon} className="w-3" alt="icone flèche" />
           </a>
+          <button className="block md:hidden ml-3">
+            <Image
+              src={assets.menu_black}
+              alt="icone menu"
+              className="w-6 cursor-pointer"
+              onClick={openMenu}
+            />
+          </button>
         </div>
+
+        {/* -- ----- menu mobile ----- -- */}
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt="close menu"
+              className="w-5 cursor-pointer"
+            />
+          </div>
+          <li>
+            <a href="#top" className="font-ovo" onClick={closeMenu}>
+              Accueil
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="font-ovo" onClick={closeMenu}>
+              A propos
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="font-ovo" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#work" className="font-ovo" onClick={closeMenu}>
+              Réalisations
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="font-ovo" onClick={closeMenu}>
+              Contact
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
